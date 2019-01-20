484f9fa9c28759e70f292ca18b79dde639b6bdea
SexyPhonenix <zhangpengsheng@focuschina.com>
2019-01-20 16:44:24 +0800

    add recommend function

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-Swiper></home-Swiper>
-    <home-Icons></home-Icons>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
+import HomeRecommend from './components/Recommend.vue'
+
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: 1,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 2,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 3,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 4,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 5,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 6,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }, {
+        id: 7,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/b3/1415e635014967c75a3d955ae256a628.water.jpg_200x200_54ee590f.jpg',
+        title: '南京汤山一号温泉',
+        desc: '生态旅游，温泉度假，收获健康好心情'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
